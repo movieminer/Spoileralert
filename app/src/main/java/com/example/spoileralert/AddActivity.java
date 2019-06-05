ea561b8db828f3bb47b4b9944cd4af9529f366a2
Julian van der Horst <julianvanderhorst@hotmail.com>
2019-06-05 14:35:14 +0200

    De add menu mooier gemaakt

--- app/src/main/java/com/example/spoileralert/AddActivity.java
+++ app/src/main/java/com/example/spoileralert/AddActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
